f123bd398a89fd96a88f4af99b50e922dfdf2e2e
Sangram Das <sangramraju143@gmail.com>
2025-08-29 19:56:00 +0530

    Starting the projects

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,25 @@
+import { createContext , useState } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AppContext = createContext()
+
+const AppContextProvider =(props) => {
+
+    const [user , setUser] = useState(null);
+
+
+    const value = {
+        user ,setUser
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+
+        </AppContext.Provider>
+    )
+
+}
+
+
+export default AppContextProvider
